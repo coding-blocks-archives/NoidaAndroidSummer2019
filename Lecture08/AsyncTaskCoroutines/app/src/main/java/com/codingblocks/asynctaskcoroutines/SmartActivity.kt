c941f59b523a0bceabe7dd688d07a6fde07e7207
Rohan Maity <rmrohan03@gmail.com>
2019-06-19 15:33:51 +0530

    Coroutines async, launch + newtwork fetch

--- Lecture08/AsyncTaskCoroutines/app/src/main/java/com/codingblocks/asynctaskcoroutines/SmartActivity.kt
+++ Lecture08/AsyncTaskCoroutines/app/src/main/java/com/codingblocks/asynctaskcoroutines/SmartActivity.kt
@@ -3,22 +3,27 @@ package com.codingblocks.asynctaskcoroutines
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.lang.StringBuilder
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.coroutines.CoroutineContext
 import kotlin.random.Random
 
-class SmartActivity : AppCompatActivity() {
+class SmartActivity : AppCompatActivity(),CoroutineScope {
 
-    val uiScope = CoroutineScope(Dispatchers.Main)
+    val superVisor = SupervisorJob()
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + superVisor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         button1.setOnClickListener {
-            uiScope.launch {
+          launch(Dispatchers.Unconfined) {
 //                for (i in 0..10){
 //                    delay(5000)
 //                    tv1.text= "$i"
@@ -27,14 +32,25 @@ class SmartActivity : AppCompatActivity() {
                 tv1.text= "After 10 sec"
             }
 
-            uiScope.launch {
+            launch {
                 delay(5000)
                 tv1.text = "After 5 sec"
             }
         }
 
         button2.setOnClickListener {
-            tv2.text = randNumber().toString()
+            launch {
+                val result:Deferred<String> = getUrlAsync("https://jsonplaceholder.typicode.com/posts")
+
+                tv1.text= "Doing work over internet"
+                delay(5000)
+
+                val result2:Deferred<String> = getUrlAsync("https://jsonplaceholder.typicode.com/")
+                delay(5000)
+                tv1.text = "Result 1 & 2 being prepared"
+
+                tv2.text = "result 1 =${result.await()[0]}, result 2 ${result2.await()[0]} "
+            }
         }
 
 
@@ -42,4 +58,43 @@ class SmartActivity : AppCompatActivity() {
 
     private fun randNumber() = Random(System.currentTimeMillis()).nextInt()
 
+    private fun getUrlAsync(urlString: String):Deferred<String>{
+        return async(Dispatchers.IO) {
+            val url = URL(urlString)
+            val connection = url.openConnection() as HttpURLConnection
+            val isr = InputStreamReader(connection.inputStream)
+            val bufferReader = BufferedReader(isr)
+            val sb = StringBuilder()
+            var buffer:String? = ""
+            while (buffer!=null){
+                sb.append(buffer)
+                buffer = bufferReader.readLine()
+            }
+
+            sb.toString()
+        }
+    }
+
+    private suspend fun get(urlString:String):String{
+        return  withContext(Dispatchers.IO){
+            val url = URL(urlString)
+            val connection = url.openConnection() as HttpURLConnection
+            val isr = InputStreamReader(connection.inputStream)
+            val bufferReader = BufferedReader(isr)
+            val sb = StringBuilder()
+            var buffer:String? = ""
+            while (buffer!=null){
+                sb.append(buffer)
+                buffer = bufferReader.readLine()
+            }
+
+            sb.toString()
+        }
+    }
+
+    override fun onDestroy() {
+        coroutineContext.cancelChildren()
+        super.onDestroy()
+
+    }
 }
