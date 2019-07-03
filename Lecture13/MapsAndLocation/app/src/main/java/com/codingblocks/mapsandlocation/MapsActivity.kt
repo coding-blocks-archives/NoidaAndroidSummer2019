905fd6cec114eff41fe542da26ff20043e163726
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-03 14:58:14 +0530

    Location Listener to Listen Updates of Location

--- Lecture13/MapsAndLocation/app/src/main/java/com/codingblocks/mapsandlocation/MapsActivity.kt
+++ Lecture13/MapsAndLocation/app/src/main/java/com/codingblocks/mapsandlocation/MapsActivity.kt
@@ -1,11 +1,16 @@
 package com.codingblocks.mapsandlocation
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -13,30 +18,70 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
-import java.util.jar.Manifest
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
+    lateinit var locMan: LocationManager
+    lateinit var locLis: LocationListener
     private lateinit var mMap: GoogleMap
+
+    override fun onLocationChanged(location: Location?) {
+        Toast.makeText(
+            this,
+            "long = ${location?.longitude}   lat = ${location?.latitude} ",
+            Toast.LENGTH_LONG
+        ).show()
+        if(mMap != null){
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location?.latitude?.let {
+                LatLng(
+                    it, location.longitude
+                )
+            }, 12f))
+        }
+    }
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        locLis = this
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-        if(ActivityCompat.checkSelfPermission(this,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE,android.Manifest.permission.ACCESS_FINE_LOCATION),1234)
-        }else{
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ),
+                1234
+            )
+        } else {
             startLocationUpdates()
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun startLocationUpdates() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        locMan = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        locMan.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+            1000,0f,locLis)
     }
 
     override fun onRequestPermissionsResult(
@@ -46,8 +91,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode == 1234){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+        if (requestCode == 1234) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+            }
         }
     }
 
@@ -64,7 +111,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        with(mMap.uiSettings){
+        with(mMap.uiSettings) {
             setAllGesturesEnabled(true)
             isZoomControlsEnabled = true
 
@@ -74,11 +121,18 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val melbourne = LatLng(-37.0, 144.0)
 
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,12f))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 12f))
 
-        mMap.addPolyline(PolylineOptions()
-            .add(sydney,melbourne)
-            .color(ContextCompat.getColor(baseContext,R.color.colorPrimary))
-            .width(2f))
+        mMap.addPolyline(
+            PolylineOptions()
+                .add(sydney, melbourne)
+                .color(ContextCompat.getColor(baseContext, R.color.colorPrimary))
+                .width(2f)
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locMan.removeUpdates(this)
     }
 }
