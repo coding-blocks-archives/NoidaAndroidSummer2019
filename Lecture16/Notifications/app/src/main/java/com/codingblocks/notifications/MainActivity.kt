e38e13ddaec66da890e70c72eb61e9f727d8ce45
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-15 15:30:28 +0530

    add pendingintent notifications

--- Lecture16/Notifications/app/src/main/java/com/codingblocks/notifications/MainActivity.kt
+++ Lecture16/Notifications/app/src/main/java/com/codingblocks/notifications/MainActivity.kt
@@ -2,11 +2,16 @@ package com.codingblocks.notifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.drm.DrmStore
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,14 +29,51 @@ class MainActivity : AppCompatActivity() {
                     NotificationManager.IMPORTANCE_DEFAULT
                 )
             )
+        btn1.setOnClickListener {
+            val simpleNotfication = NotificationCompat.Builder(this, "first")
+                .setContentTitle("Simple Title")
+                .setContentText("This is sample description of a simple notification")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
+            nm.notify(System.currentTimeMillis().toInt(), simpleNotfication)
+        }
 
-        val simpleNotfication = NotificationCompat.Builder(this,"first")
-            .setContentTitle("Simple Title")
-            .setContentText("This is sample description of a simple notification")
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .build()
-        nm.notify(System.currentTimeMillis().toInt(),simpleNotfication)
+        btn2.setOnClickListener {
+            val i = Intent()
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("https://www.google.com")
+
+            val pi = PendingIntent.getActivity(this, 123, i, PendingIntent.FLAG_UPDATE_CURRENT)
+            val simpleNotfication = NotificationCompat.Builder(this, "first")
+                .setContentTitle("Simple Title")
+                .setContentText("This is sample description of a simple notification")
+                .setContentIntent(pi)
+                .setOngoing(true)
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
+            nm.notify(System.currentTimeMillis().toInt(), simpleNotfication)
+        }
+
+        btn3.setOnClickListener {
+            val i = Intent()
+            i.action = Intent.ACTION_VIEW
+            i.data = Uri.parse("https://www.google.com")
+
+            val pi = PendingIntent.getActivity(this, 123, i, PendingIntent.FLAG_UPDATE_CURRENT)
+            val simpleNotfication = NotificationCompat.Builder(this, "first")
+                .setContentTitle("Simple Title")
+                .setContentText("This is sample description of a simple notification")
+                .addAction(R.drawable.ic_launcher_foreground,"Click ME",pi)
+                .setOngoing(true)
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .build()
+            nm.notify(System.currentTimeMillis().toInt(), simpleNotfication)
+        }
 
 
 
