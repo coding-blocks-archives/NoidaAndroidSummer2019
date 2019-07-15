cb26be152e5345ebe03d9de9fabfac813b251ca5
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-15 13:55:56 +0530

    Write to downloads directory

--- Lecture16/FileReadWrite/app/src/main/java/com/codingblocks/filereadwrite/MainActivity.kt
+++ Lecture16/FileReadWrite/app/src/main/java/com/codingblocks/filereadwrite/MainActivity.kt
@@ -2,6 +2,7 @@ package com.codingblocks.filereadwrite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
@@ -12,8 +13,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i("FILES", "Files dir ${filesDir.path}")
-        val file = File(filesDir, "myfile.text")
+        Log.i("FILES", "Files dir ${getExternalFilesDir(Environment.getDataDirectory().absolutePath)}")
+
+        //System App Directory -  No need to ask for permissions
+//        val file = File(getExternalFilesDir(Environment.getDataDirectory().absolutePath), "myfile.text")
+        //Downloads Directory - Need to ask permissions for this
+        val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "myfile.text")
 
         btn.setOnClickListener {
             //            file.writeText(edtv.text.toString())
