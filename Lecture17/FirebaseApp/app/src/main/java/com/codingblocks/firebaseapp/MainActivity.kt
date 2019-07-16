f5b124ad2ada6f92479211a718eb493eef29d04e
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-16 14:40:52 +0530

    add auth state listener,signout and login process

--- Lecture17/FirebaseApp/app/src/main/java/com/codingblocks/firebaseapp/MainActivity.kt
+++ Lecture17/FirebaseApp/app/src/main/java/com/codingblocks/firebaseapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.codingblocks.firebaseapp
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -15,6 +16,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth.addAuthStateListener {
+            if (it.currentUser != null) {
+                btn.isVisible = false
+                btn2.isVisible = true
+            } else {
+                btn.isVisible = true
+                btn2.isVisible = false
+            }
+        }
+
+        btn2.setOnClickListener {
+            auth.signOut()
+        }
         btn.setOnClickListener {
             if (email.editText?.text.isNullOrEmpty()) {
                 email.isErrorEnabled = true
@@ -35,9 +50,27 @@ class MainActivity : AppCompatActivity() {
                 }.addOnFailureListener {
                     btn.isEnabled = true
                     Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+                    if (it.localizedMessage.contains("use", true)) {
+                        loginUser()
+                    }
                 }
             }
         }
 
+    }
+
+    private fun loginUser() {
+        auth.signInWithEmailAndPassword(
+            email.editText?.text.toString(),
+            password.editText?.text.toString()
+        ).addOnCompleteListener {
+            btn.isEnabled = false
+        }.addOnSuccessListener {
+            btn.isEnabled = true
+            Toast.makeText(this, "Logged In Succesfully", Toast.LENGTH_LONG).show()
+        }.addOnFailureListener {
+            btn.isEnabled = true
+            Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+        }
     }
 }
