b09475f2e5ba091acfb8f1bdee00424389055071
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-16 15:09:00 +0530

    Auth Verification on Otp

--- Lecture17/FirebaseApp/app/src/main/java/com/codingblocks/firebaseapp/MainActivity.kt
+++ Lecture17/FirebaseApp/app/src/main/java/com/codingblocks/firebaseapp/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import com.google.firebase.FirebaseException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,31 +34,69 @@ class MainActivity : AppCompatActivity() {
         btn2.setOnClickListener {
             auth.signOut()
         }
-        btn.setOnClickListener {
-            if (email.editText?.text.isNullOrEmpty()) {
-                email.isErrorEnabled = true
-                email.error = "Cannot be Empty"
-            } else if (password.editText?.text.isNullOrEmpty()) {
-                password.isErrorEnabled = true
-                password.error = "Cannot be Empty"
-            } else {
-                auth.createUserWithEmailAndPassword(
-                    email.editText?.text.toString(),
-                    password.editText?.text.toString()
-                ).addOnCompleteListener {
-                    btn.isEnabled = false
-                }.addOnSuccessListener {
-                    btn.isEnabled = true
-                    Toast.makeText(this, "Account Created Succesfully", Toast.LENGTH_LONG).show()
 
-                }.addOnFailureListener {
-                    btn.isEnabled = true
-                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
-                    if (it.localizedMessage.contains("use", true)) {
-                        loginUser()
-                    }
-                }
+        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            override fun onVerificationCompleted(p0: PhoneAuthCredential?) {
+                Toast.makeText(this@MainActivity, "Verification Completed", Toast.LENGTH_LONG).show()
+                createAccount(p0)
             }
+
+            override fun onVerificationFailed(p0: FirebaseException?) {
+                Toast.makeText(this@MainActivity, "Verification Failed", Toast.LENGTH_LONG).show()
+
+            }
+
+            override fun onCodeSent(p0: String?, p1: PhoneAuthProvider.ForceResendingToken?) {
+                super.onCodeSent(p0, p1)
+                Toast.makeText(this@MainActivity, "Code Sent", Toast.LENGTH_LONG).show()
+
+            }
+
+        }
+        btn.setOnClickListener {
+            //            if (email.editText?.text.isNullOrEmpty()) {
+//                email.isErrorEnabled = true
+//                email.error = "Cannot be Empty"
+//            } else if (password.editText?.text.isNullOrEmpty()) {
+//                password.isErrorEnabled = true
+//                password.error = "Cannot be Empty"
+//            } else {
+//                auth.createUserWithEmailAndPassword(
+//                    email.editText?.text.toString(),
+//                    password.editText?.text.toString()
+//                ).addOnCompleteListener {
+//                    btn.isEnabled = false
+//                }.addOnSuccessListener {
+//                    btn.isEnabled = true
+//                    Toast.makeText(this, "Account Created Succesfully", Toast.LENGTH_LONG).show()
+//
+//                }.addOnFailureListener {
+//                    btn.isEnabled = true
+//                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+//                    if (it.localizedMessage.contains("use", true)) {
+//                        loginUser()
+//                    }
+//                }
+//            }
+
+            PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                "+91${email.editText?.text}", // Phone number to verify
+                60, // Timeout duration
+                TimeUnit.SECONDS, // Unit of timeout
+                this, // Activity (for callback binding)
+                callbacks
+            ) // OnVerificationStateChangedCallbacks
+        }
+
+    }
+
+    private fun createAccount(p0: PhoneAuthCredential?) {
+        auth.signInWithCredential(p0!!).addOnCompleteListener {
+
+        }.addOnSuccessListener {
+
+        }.addOnFailureListener {
+
         }
 
     }
