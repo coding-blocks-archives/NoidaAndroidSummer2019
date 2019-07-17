bb7fbcb97d682006f3648c236170acb37c8f8b11
pulkit-mac <aggarwalpulkit596@gmail.com>
2019-07-17 14:06:22 +0530

    add child event listener with model class

--- Lecture18/FirebaseDatabase/app/src/main/java/com/codingblocks/firebasedatabase/MainActivity.kt
+++ Lecture18/FirebaseDatabase/app/src/main/java/com/codingblocks/firebasedatabase/MainActivity.kt
@@ -2,10 +2,10 @@ package com.codingblocks.firebasedatabase
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,16 +20,30 @@ class MainActivity : AppCompatActivity() {
 
         db.child("messages")
             .child("user1")
-            .child("chat1")
-            .child("text")
-            .addValueEventListener(object : ValueEventListener {
+            .addChildEventListener(object : ChildEventListener {
                 override fun onCancelled(p0: DatabaseError) {
                 }
 
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    tv.text = snapshot.value.toString()
+                override fun onChildMoved(p0: DataSnapshot, p1: String?) {
+                }
+
+                override fun onChildChanged(p0: DataSnapshot, p1: String?) {
+                }
+
+                override fun onChildAdded(snapshot: DataSnapshot, p1: String?) {
+                    tv.text = "${tv.text}  ${snapshot.getValue(Todo::class.java)?.text}"
+
+                }
+
+                override fun onChildRemoved(p0: DataSnapshot) {
                 }
 
             })
+
     }
+}
+
+class Todo {
+    var text: String = ""
+    var time: String = ""
 }
